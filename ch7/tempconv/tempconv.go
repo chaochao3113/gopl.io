b315f322ecb2660533628ec6e382cb7d85c1c09d
丁延超 <dingyc10@chinaunicom.cn>
2019-08-27 21:10:09 +0800

    init ch7

---
+++ ch7/tempconv/tempconv.go
@@ -0,0 +1,45 @@
+package tempconv
+
+import "fmt"
+
+type Celsius float64
+
+type Fahrenheit float64
+
+type celsiusFlag struct {
+	Celsius
+}
+
+const (
+	AbsoluteZeroC Celsius = -273.15
+	FreezingC Celsius = 0
+	BoilingC Celsius = 100
+)
+
+func CToF(c Celsius) Fahrenheit {
+	return Fahrenheit(c*9/5 + 32)
+}
+
+func FToC(f Fahrenheit) Celsius {
+	return Celsius((f - 32) * 5 / 9)
+}
+
+func (c *Celsius) String() string {
+	return fmt.Sprintf("%g°C", c)
+}
+
+func (f *celsiusFlag) Set(s string) error {
+	var unit string
+	var value float64
+	fmt.Sscanf(s, "%f%s", &value, &unit)
+	switch unit {
+	case "C", "°C":
+		f.Celsius = Celsius(value)
+		return nil
+	case "F", "°F":
+		f.Celsius = FToC(Fahrenheit(value))
+		return nil
+	}
+	return fmt.Errorf("invalid temperature %q", s)
+
+}
